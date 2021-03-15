3b5efd59dfaa473128ef444ba0c6763aeabd7ec0
wright <wright.huang@baiyitech.com.tw>
2021-03-15 19:28:57 +0800

    feat: create master

---
+++ Assets/script/background.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+	public GameObject Emeny; //宣告物件，名稱Emeny
+	public float time; //宣告浮點數，名稱time
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		time += Time.deltaTime; //時間增加
+		
+		if (time > 0.5f) //如果時間大於0.5(秒)
+		{
+			// 宣告位置pos，Random.Range(-2.5f,2.5f)代表X是2.5到-2.5之間隨機
+			Vector3 pos = new Vector3(Random.Range(-5f, 5f), 4.5f, 0); 
+
+			// 產生敵人
+			Instantiate(Emeny, pos, transform.rotation); 
+
+			//時間歸零
+			time = 0f; 
+		}
+
+	}
+}
---
+++ Assets/script/shipControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipControl : MonoBehaviour
+{
+    public GameObject Bullet;
+    public float time; //宣告浮點數，名稱time
+    public float fireSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.moveHandle();
+        this.fireHandle();
+    }
+
+    private void moveHandle()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            gameObject.transform.position += new Vector3(0.1f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            gameObject.transform.position -= new Vector3(0.1f, 0, 0);
+        }
+    }
+
+    private void fireHandle() 
+    {
+        // 連發
+        time += Time.deltaTime; //時間增加
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (time > fireSpeed)
+            {
+                Vector3 pos = gameObject.transform.position + new Vector3(0, 0.3f, 0);
+                Instantiate(Bullet, pos, gameObject.transform.rotation);
+
+                //時間歸零
+                time = 0f;
+            }
+        }
+
+        if (time > 10f) 
+        {
+            time = 0f;
+        }
+    }
+}
