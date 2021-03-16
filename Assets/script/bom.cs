c43ac39f017547be672a7fb023d802a1ee718e6a
wright <wright.huang@baiyitech.com.tw>
2021-03-16 18:33:44 +0800

    feat: add bom animator

---
+++ Assets/script/bom.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bom : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void AnimationEnd()
+	{
+		Destroy (gameObject); //消滅物件
+	}
+}
--- Assets/script/monster.cs
+++ Assets/script/monster.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class monster : MonoBehaviour
 {
+	public GameObject explo; //宣告一個名為explo的物件
+	public GameObject bigexplo; //宣告一個名為explo的物件
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -22,6 +25,17 @@ public class monster : MonoBehaviour
 		{
 			Destroy(col.gameObject); //消滅碰撞的物件
 			Destroy(gameObject); //消滅物件本身
+			if (col.tag == "Bullet") //如果碰撞的標籤是Ship
+			{
+				Instantiate(explo, col.gameObject.transform.position, col.gameObject.transform.rotation);
+				//在碰撞物件的位置產生爆炸，也就是在太空船的位置產生爆炸
+			}
+			if (col.tag == "Player") //如果碰撞的標籤是Ship
+			{
+				Instantiate(bigexplo, col.gameObject.transform.position, col.gameObject.transform.rotation);
+				//在碰撞物件的位置產生爆炸，也就是在太空船的位置產生爆炸
+			}
 		}
+
 	}
 }
