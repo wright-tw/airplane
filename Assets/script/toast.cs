a2c14248abed64a654ebc9961ec834c44cf22629
wright <wright.huang@baiyitech.com.tw>
2021-04-16 19:11:43 +0800

    feat: add plugin

---
+++ Assets/script/toast.cs
@@ -0,0 +1,21 @@
+using DG.Tweening;
+using UnityEngine.UI; //使用UI
+using UnityEngine;
+
+public class Toast
+{
+	public static void alert(GameObject obj, string msg)
+	{
+		CanvasGroup tipCanvasGroup = obj.GetComponentInChildren<CanvasGroup>();
+		tipCanvasGroup.GetComponentInChildren<Text>().text = msg;
+		tipCanvasGroup.alpha = 1;
+		
+		DOTween.Kill(tipCanvasGroup);
+
+		Sequence sequence = DOTween.Sequence();
+		sequence.target = tipCanvasGroup;
+		sequence.Append(tipCanvasGroup.DOFade(1, 0.8f));
+		sequence.AppendInterval(0.5f);
+		sequence.Append(tipCanvasGroup.DOFade(0, 1.0f));
+	}
+}
